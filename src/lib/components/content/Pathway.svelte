d937e75e57fc1e1064e071aefa3d3a9eea35a165
jam.jar <darkestknight3d@outlook.com>
2025-09-25 20:37:45 -0700

    no more signups

--- src/lib/components/content/Pathway.svelte
+++ src/lib/components/content/Pathway.svelte
@@ -369,19 +369,7 @@
 
 	<img src="/daydream-media/paper-airplane.png" alt="Paper airplane" class="h-16 absolute" id="paper-airplane" />
 
-	<div class="flex flex-col items-center w-max basis-1/2 max-md:basis-full max-md:w-full z-10">
-		<div class="relative translate-y-8 max-md:translate-y-4 z-30">
-			<img src="/daydream-media/letter-1-front.png" alt="" class="object-contain absolute -bottom-16 -left-13 w-28 h-28 animate-hover ![--hover:-0.25rem] ![animation-delay:0.7s] z-20" />
-			<img src="/daydream-media/letter-1-back.png" alt="" class="object-contain absolute -bottom-16 -left-13 w-28 h-28 animate-hover ![--hover:-0.25rem] ![animation-delay:0.7s] z-10" />
-			<div class="relative w-72 h-40 max-md:w-80 animate-hover ![--hover:-0.15rem] ![animation-delay:1.7s] z-20" data-point="1">
-				<img src="/daydream-media/paper1.png" alt="" class="w-full h-full object-contain" />
-				<div class="absolute inset-0 justify-center text-center p-6 text-xl font-serif max-md:text-lg text-[#8B4513] inline-block content-center">
-					<span class="font-sans text-[#E472AB] font-bold text-[1.3rem] mr-1">#1:</span> <a href={signupUrl} class="underline">Sign up</a> for Daydream {eventName}
-				</div>
-			</div>
-		</div>
-		<img src="/daydream-media/island-1.png" alt="" class="w-72 h-72 object-contain max-md:w-64 max-md:h-64 animate-hover ![--hover:-0.25rem] ![animation-delay:0.8s] z-0" />
-	</div>
+
 
 	<div class="flex flex-col items-center w-max basis-1/2 max-md:basis-full max-md:w-full translate-y-24 max-md:translate-y-8 z-10">
 		<div class="relative translate-y-24 max-md:translate-y-4 z-30">
--- src/lib/components/landing/ParticipantSignUp.svelte
+++ src/lib/components/landing/ParticipantSignUp.svelte
@@ -14,18 +14,6 @@
 </script>
 
 <div class="mt-6 flex flex-col items-center gap-3 z-5 max-md:scale-90">
-	<div class="relative rounded-full overflow-hidden" style="padding: 2px 2px 5px 2px;">
-		<button
-			on:click={handleClick}
-			class="rounded-full bg-white border-2 border-dark font-sans p-2 flex flex-row items-center gap-2 shadow-[0_3px_0_0_theme(colors.dark)] hover:border-pink hover:shadow-[0_3px_0_0_#E472AB] active:border-dark active:shadow-[0_3px_0_0_theme(colors.dark)] active:transform active:translate-y-0.5 transition-all duration-100 cursor-pointer w-80"
-		>
-			<span class="text-dark font-sans flex-1 text-left px-3">Register for Daydream</span>
-			<div class="bg-light h-full px-5 py-[0.45rem] rounded-full border-b-2 border-[#B3866A] flex-shrink-0">
-				<img src="daydream-media/submit.svg" alt="Go" />
-			</div>
-		</button>
-	</div>
-
 	<a
 		href="https://forms.hackclub.com/daydream-stickers"
 		target="_blank"
